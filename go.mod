f2246766c4f3eff2d1a51f4e15e3f2325d4cf74c
Diego M <dmalload@asu.edu>
2025-09-19 17:17:29 -0700

    module-based installation

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/icwells/compOncDB
+
+go 1.25.1
+
+require (
+	github.com/alecthomas/kingpin/v2 v2.4.0
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/schema v1.4.1
+	github.com/gorilla/securecookie v1.1.2
+	github.com/gorilla/sessions v1.4.0
+	github.com/icwells/dbIO v1.3.6
+	github.com/icwells/go-tools v0.0.0-20220308165833-04a4f9fe5e88
+	github.com/icwells/simpleset v0.0.0-20210429220422-0ace014398a0
+	github.com/lithammer/fuzzysearch v1.1.8
+	github.com/trustmaster/go-aspell v0.0.0-20200701131845-c2b1f55bec8f
+)
+
+require (
+	github.com/Songmu/prompter v0.5.1 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- install.sh
+++ install.sh
@@ -3,30 +3,38 @@
 ##############################################################################
 # This script will install scripts for the compOncDB package.
 # 
-# Required programs:	Go 1.11+
+# Required programs:	Go 1.16+
 ##############################################################################
 
 MAIN="componcdb"
 
-installPackages () {
-	echo "Installing dependencies..."
-	ETREE="github.com/beevik/etree"
-	DATAFRAME="github.com/icwells/go-tools/dataframe"
-	FRACTION="github.com/icwells/go-tools/fraction"
-	IOTOOLS="github.com/icwells/go-tools/iotools"
-	STRARRAY="github.com/icwells/go-tools/strarray"
-	KINGPIN="github.com/alecthomas/kingpin/v2"
-	MUX="github.com/gorilla/mux"
-	SCHEMA="github.com/gorilla/schema"
-	COOKIE="github.com/gorilla/securecookie"
-	SEESSIONS="github.com/gorilla/sessions"
-	DBIO="github.com/icwells/dbIO"
-	SIMPLESET="github.com/icwells/simpleset"
-	FUZZY="github.com/lithammer/fuzzysearch/fuzzy"
-	ASPELL="github.com/trustmaster/go-aspell"
-	for I in $ETREE $DATAFRAME $FRACTION $IOTOOLS $STRARRAY $KINGPIN $MUX $SCHEMA $COOKIE $SEESSIONS $DBIO $SIMPLESET $FUZZY $ASPELL; do
+prepareModule () {
+	echo "Setting go.mod"
+	go mod init github.com/icwells/compOncDB
+	MYSQL="github.com/go-sql-driver/mysql@v1.5"
+#	ETREE="github.com/beevik/etree"
+#	DATAFRAME="github.com/icwells/go-tools/dataframe"
+#	FRACTION="github.com/icwells/go-tools/fraction"
+#	IOTOOLS="github.com/icwells/go-tools/iotools"
+#	STRARRAY="github.com/icwells/go-tools/strarray"
+#	KINGPIN="github.com/alecthomas/kingpin/v2"
+#	MUX="github.com/gorilla/mux"
+#	SCHEMA="github.com/gorilla/schema"
+#	COOKIE="github.com/gorilla/securecookie"
+#	SEESSIONS="github.com/gorilla/sessions"
+#	DBIO="github.com/icwells/dbIO"
+#	SIMPLESET="github.com/icwells/simpleset"
+#	FUZZY="github.com/lithammer/fuzzysearch/fuzzy"
+#	ASPELL="github.com/trustmaster/go-aspell"
+	deps=($MYSQL)
+	for I in $deps; do
 		go get $I
 	done
+	echo ""
+	go mod tidy
+}
+
+installPackages () {
 	for I in scikit-learn tensorflow tensorflow_hub; do
 		pip install $I
 	done
@@ -34,8 +42,8 @@ installPackages () {
 
 installMain () {
 	# compOncDB 
-	echo "Building $MAIN..."
-	go build -i -o $GOBIN/$MAIN src/*.go
+	echo "Installing $MAIN and its dependencies..."
+	go install ./...
 	echo ""
 }
 
@@ -46,17 +54,22 @@ echo ""
 
 if [ $# -eq 0 ]; then
 	installMain
+elif [ $1 = "module" ]; then
+	prepareModule
 elif [ $1 = "main" ]; then
 	installMain
 elif [ $1 = "all" ]; then
+	if [ ! -f go.mod ]; then
+		prepareModule
+	fi
 	installPackages
 	installMain
 elif [ $1 = "help" ]; then
 	echo "Installs Go scripts for compOnDB"
 	echo ""
 	echo "main	Installs scripts to GOBIN only."
-	echo "app	Installs web application only."
-	echo "all	Installs all scripts and dependencies."
+	echo "module	Prepares the module (for development only)"
+	echo "all	Prepares the module if needed, installs all scripts and dependencies."
 	echo "help	Prints help text and exits."
 	echo ""
 fi
